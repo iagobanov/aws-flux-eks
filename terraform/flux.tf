12327e611b3bc7beba15e19ced4227d2c20e9bf8
Iago Banov <iagobanov@outlook.com>
2022-12-05 12:41:22 -0300

    ADd flux

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -1,41 +1,41 @@
-# terraform {
-#   required_version = ">= 0.13"
+terraform {
+  required_version = ">= 0.13"
 
-#   required_providers {
-#     kubernetes = {
-#       source  = "hashicorp/kubernetes"
-#       version = ">= 2.0.2"
-#     }
-#     kubectl = {
-#       source  = "gavinbunney/kubectl"
-#       version = ">= 1.10.0"
-#     }
-#   }
-# }
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+  }
+}
 
 
-# provider "kubectl" {}
+provider "kubectl" {}
 
-# locals {
-#   envs = ["dev", "homol", "prod", "flux-system"]
-# }
-# resource "kubernetes_namespace" "envs" {
-#   for_each = toset(local.envs)
-#   metadata {
-#     name = each.key
-#   }
+locals {
+  envs = ["dev", "homol", "prod", "flux-system"]
+}
+resource "kubernetes_namespace" "envs" {
+  for_each = toset(local.envs)
+  metadata {
+    name = each.key
+  }
 
-#   lifecycle {
-#     ignore_changes = [
-#       metadata[0].labels,
-#     ]
-#   }
-# }
+  lifecycle {
+    ignore_changes = [
+      metadata[0].labels,
+    ]
+  }
+}
 
-# resource "null_resource" "flux_bootstrap" {
-#   provisioner "local-exec" {
-#     command = "flux bootstrap github --owner=${var.github_owner} --repository=${var.repository_name} --path=./cluster/ --read-write-key --branch=main --namespace=flux-system --components-extra=image-reflector-controller,image-automation-controller"
-#   }
+resource "null_resource" "flux_bootstrap" {
+  provisioner "local-exec" {
+    command = "flux bootstrap github --owner=${var.github_owner} --repository=${var.repository_name} --path=./cluster/ --read-write-key --branch=main --namespace=flux-system --components-extra=image-reflector-controller,image-automation-controller"
+  }
 
-#   depends_on = [module.eks_blueprints]
-# }
+  depends_on = [module.eks_blueprints]
+}
